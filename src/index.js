04b670a2404a297cb02ed2854f9422e96f6ab141
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-03-15 00:04:41 +0200

    corrects errors on the mentor's recommendation

--- src/index.js
+++ src/index.js
@@ -32,43 +32,42 @@ function getURL(searchParams) {
 //   input.value = '';
 // }
 
-function getImages(URL) {
-  return axios.get(URL)
-    .then(response => {
-      const images = response.data.hits.map(hit => ({
-        webformatURL: hit.webformatURL,
-        largeImageURL: hit.largeImageURL,
-        tags: hit.tags,
-        likes: hit.likes,
-        views: hit.views,
-        comments: hit.comments,
-        downloads: hit.downloads,
-      }));
-      
-      if (images.length === 0) {
-        Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-      } else {
-        galleryEl.innerHTML = renderGallery(images);
-        loadMoreBtn.style.display = 'block';
-        const totalHits = response.data.totalHits;
-        const currentPage = Number(searchParams.get('page'));
-        const perPage = Number(searchParams.get('per_page'));
-        const totalPages = Math.ceil(totalHits / perPage);
-        const displayedImages = (currentPage - 1) * perPage + images.length;
-        if (displayedImages >= totalHits) {
-          loadMoreBtn.style.display = 'none';
-          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-        }
-        console.log(totalHits);
+async function getImages(URL) {
+  try {
+    const response = await axios.get(URL);
+    const images = response.data.hits.map(hit => ({
+      webformatURL: hit.webformatURL,
+      largeImageURL: hit.largeImageURL,
+      tags: hit.tags,
+      likes: hit.likes,
+      views: hit.views,
+      comments: hit.comments,
+      downloads: hit.downloads,
+    }));
+
+    if (images.length === 0) {
+      Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+    } else {
+      galleryEl.innerHTML = renderGallery(images);
+      loadMoreBtn.style.display = 'block';
+      const totalHits = response.data.totalHits;
+      const currentPage = Number(searchParams.get('page'));
+      const perPage = Number(searchParams.get('per_page'));
+      const displayedImages = (currentPage - 1) * perPage + images.length;
+      if (displayedImages >= totalHits) {
+        loadMoreBtn.style.display = 'none';
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
       }
-      
-      return images;
-    })
-    .catch((error) => {
-      console.log(error);
-      Notiflix.Notify.failure('Error fetching images');
-    });
+      console.log(totalHits);
+    }
+    
+    return images;
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure('Error fetching images');
+  }
 }
+
 
 function handleSubmit(event, input, searchParams) {
   event.preventDefault();
@@ -123,16 +122,27 @@ function renderGallery(images) {
 
 loadMoreBtn.addEventListener('click', loadMoreImages);
 
-function loadMoreImages() {
+async function loadMoreImages() {
   currentPage += 1;
-  searchParams.set('page', currentPage);
+  searchParams.set('page', currentPage); // update the 'page' parameter
   const URL = getURL(searchParams);
-  getImages(URL)
-    .then(images => {
-      galleryEl.insertAdjacentHTML('beforeend', renderGallery(images));
-      if (images.length < 40) {
-        loadMoreBtn.style.display = 'none';
-      }
-    })
-    .catch(error => console.log(error));
+  try {
+    const response = await axios.get(URL);
+    const images = response.data.hits.map(hit => ({
+      webformatURL: hit.webformatURL,
+      largeImageURL: hit.largeImageURL,
+      tags: hit.tags,
+      likes: hit.likes,
+      views: hit.views,
+      comments: hit.comments,
+      downloads: hit.downloads,
+    }));
+
+    galleryEl.insertAdjacentHTML('beforeend', renderGallery(images));
+    if (images.length < searchParams.get('per_page')) {
+      loadMoreBtn.style.display = 'none';
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
